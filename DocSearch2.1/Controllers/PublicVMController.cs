8e3b24dffe2bb6fb3339be23e8492eadbaf29b90
jackmtr <jackmtr@hotmail.com>
2017-04-05 12:15:08 -0700

    added publicVM class, that will do the main data display (before adding the repo and methods)

---
+++ DocSearch2.1/Controllers/PublicVMController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocSearch2._1.Controllers
+{
+    public class PublicVMController : Controller
+    {
+        //will be grabbing data from 5 tables in db: Folder, Document, DocumentType, Category, and DocumentReference
+
+        // dunno if need this: [AcceptVerbs(HttpVerbs.Get)]
+        // GET: PublicVM
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DocSearch2.1/ViewModels/PublicVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocSearch2._1.ViewModels
+{
+    public class PublicVM
+    {
+        //folder_id
+        public int Folder_ID { get; set; } //tbl_Folder
+        //document_id
+        public int Document_ID { get; set; } //tbl_Document
+
+        //documentType_id
+        public int DocumentType_ID { get; set; } //tbl_Document or tbl_DocumentType
+
+        public string DocumentTypeName { get; set; } //tbl_DocumentType as Name
+
+        //document.issue_DT
+        public System.DateTime IssueDate { get; set; } //tbl_Document
+
+        //document.description
+        public string Description { get; set; } //tbl_Document
+
+        //category_id
+        public int Category_ID { get; set; } //tbl_DocumentType or tbl_Category
+
+        //category.name
+        public string CategoryName { get; set; } //tbl_Category as Name
+
+        //docReference.Date1_DT
+        public System.DateTime EffectiveDate { get; set; } //tbl_DocReference as Date1_DT
+
+        //file stuff properties need to be added
+    }
+}
