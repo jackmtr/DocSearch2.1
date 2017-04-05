988f34c12fefcd9a66b88c7f0cd8c65b7f27581a
jackmtr <jackmtr@hotmail.com>
2017-04-05 10:50:12 -0700

    Remake of original DocSearch.  Better MVC Repository Design.  Basic Folder Search enabled.

---
+++ DocSearch2.1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocSearch2._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
